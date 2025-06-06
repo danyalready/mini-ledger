8c463e31163758485a96086a36796b0d72b86526
Doniyor <doniyorgit@gmail.com>
2025-06-06 16:31:48 +0500

    feat: add wallet class

---
+++ src/blockchain/Wallet.ts
@@ -0,0 +1,23 @@
+import * as crypto from 'crypto';
+
+export class Wallet {
+    private privateKey: crypto.KeyObject;
+    private publicKey: string;
+
+    constructor() {
+        const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
+            namedCurve: 'secp256k1',
+        });
+
+        this.privateKey = privateKey;
+        this.publicKey = publicKey.export({ type: 'spki', format: 'pem' }).toString();
+    }
+
+    getAddress(): string {
+        return this.publicKey;
+    }
+
+    getPrivateKey(): crypto.KeyObject {
+        return this.privateKey;
+    }
+}
